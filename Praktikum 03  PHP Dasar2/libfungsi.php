201c7d204a2a976e0a3bb334fbf95e745d27bcd3
salmayh <salsaniy21@gmail.com>
2022-03-20 00:42:04 +0700

    20/3

---
+++ Praktikum 03  PHP Dasar2/libfungsi.php
@@ -0,0 +1,25 @@
+<?php
+function kelulusan($_nilai) {
+    if ($_nilai > 55 ){
+        return 'LULUS';
+    }else {
+        return 'TIDAK LULUS';
+    }
+}
+?>
+
+<!-- <?php
+
+function kelulusan($_nilai) {
+// isi fungsi kelulusan
+
+}
+function grade($_nilai) {
+// isi fungsi grade
+}
+
+ function predikat($_grade) // argumen nya E,D,C,B,A
+{ 
+// isi fungsi predikat
+}
+?> -->
---
+++ Praktikum 03  PHP Dasar2/nilai_siswa.php
@@ -0,0 +1,8 @@
+<?php
+// letakan di paling atas file
+require_once 'libfungsi.php';
+// kode lainnya logik program anda
+// $_nilai = total nilai
+$hasil_ujian = kelulusan($_nilai)
+echo 'DINYATAKAN ' . $hasil_ujian;
+?>
